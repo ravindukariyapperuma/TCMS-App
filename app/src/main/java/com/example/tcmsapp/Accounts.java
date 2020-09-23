ac54df1c9d777418458d78555d62579588e0dc97
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-09-23 22:13:25 +0530

    create-dashboard

--- app/src/main/java/com/example/tcmsapp/Accounts.java
+++ app/src/main/java/com/example/tcmsapp/Accounts.java
@@ -2,13 +2,26 @@ package com.example.tcmsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Accounts extends AppCompatActivity {
+    ImageButton btnTeacherLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
+
+        btnTeacherLogin = findViewById(R.id.btnTeacherLogin);
+        btnTeacherLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
